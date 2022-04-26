b63e70561f19abe42512497409d9b09fec24331a
Vio Karina <55702992+viokarina@users.noreply.github.com>
2022-04-26 10:03:42 +0700

    Add right intro

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -3,6 +3,13 @@ import './Intro.css'
 import Github from '../../img/github.png'
 import Linkedin from '../../img/linkedin.png'
 import Instagram from '../../img/instagram.png'
+import Vector1 from '../../img/Vector1.png'
+import Vector2 from '../../img/Vector2.png'
+import vio from '../../img/vio.png'
+import thumbup from '../../img/thumbup.png'
+import crown from '../../img/crown.png'
+import glassesimoji from '../../img/glassesimoji.png'
+
 
 const Intro = () => {
     return (
@@ -21,8 +28,9 @@ const Intro = () => {
                 </div>
             </div>
             <div className="i-right">
-
-
+                <img src={Vector1} alt="" />
+                <img src={Vector2} alt="" />
+                <img src={vio} alt="" />
             </div>
         </div>
     )
